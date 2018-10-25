98d18dd175d32e69c262542347091bd7142a238d
ysmiraak <ysmiraak@gmail.com>
2018-10-25 10:24:15 +0000

    big data

--- src/stats.py
+++ src/stats.py
@@ -1,11 +1,11 @@
 from collections import Counter
 from util import diter
-from util_io import load
+from util_io import load_txt
 import matplotlib.pyplot as plt
 import numpy as np
 
 path = "../data/europarl-v7.de-en.de"
-corp = list(load(path))
+corp = list(load_txt(path))
 
 print(len(corp), "sentences")
 
@@ -19,3 +19,39 @@ for i in range(11):
 alphabet = Counter(diter(corp))
 
 print(len(alphabet), "characters")
+
+
+
+
+from collections import Counter
+from trial import config as C
+from util_io import clean_load
+import numpy as np
+
+vocab, count = Counter(), 0
+for s, t in clean_load("/data/wmt/de-en/corpus", C.cap_src, C.cap_tgt):
+    vocab.update(s.split())
+    vocab.update(t.split())
+    count += 1
+    if count >= 2**24:
+        break
+
+with open("../trial/vocab", 'w') as lines:
+    for w, n in vocab.most_common():
+        print(w, n, sep= "\t", file= f)
+
+vocab = Counter()
+with open("../trial/vocab") as lines:
+    for line in lines:
+        w, n = line.split()
+        vocab[w] = int(n)
+
+from util_io import vocab as chars, save_pkl, load_pkl
+
+alphabet = Counter()
+for w, n in vocab.items():
+    alphabet.update({c: n for c in w})
+
+index = "".join(chars(alphabet, specials= "\xa0\n "))
+save_pkl("../trial/data/index.pickle", index)
+index = load_pkl("../trial/data/index.pickle")
--- src/util_io.py
+++ src/util_io.py
@@ -1,19 +1,58 @@
 from collections import Counter
 from itertools import chain
+import pickle
+import re
 
 
-def load(filename):
-    """yields lines."""
+def clean(s, p= re.compile(r"&#91;|&#93;|&amp;|&apos;|&gt;|&lt;|&quot;|@-@|� s")
+          , tr= lambda m, t= {
+              "� s": "'s"
+              , "&#91;": "["
+              , "&#93;": "]"
+              , "&amp;": "&"
+              , "&apos;": "'"
+              , "&gt;": ">"
+              , "&lt;": "<"
+              , "&quot;": "\""
+              , "@-@": "-"}: t[m.group()]):
+    return p.sub(tr, s)
+
+
+def clean_load(filename, cap_src, cap_tgt):
+    with open(filename) as lines:
+        for line in lines:
+            s, t, _ = clean(line).split("\t")
+            if not s or not t \
+               or cap_src < len(s) \
+               or cap_tgt < len(t):
+                continue
+            yield s, t
+
+
+def load_txt(filename):
+    """yields lines from text file."""
     with open(filename) as file:
         for line in file:
             yield line.strip()
 
 
-def save(filename, lines):
-    """writes lines."""
+def save_txt(filename, lines):
+    """writes lines to text file."""
     with open(filename, 'w') as file:
         for line in lines:
             print(line, file= file)
+
+
+def load_pkl(filename):
+    """loads pickle file."""
+    with open(filename, 'rb') as dump:
+        return pickle.load(dump)
+
+
+def save_pkl(filename, obj):
+    "saves to pickle file."
+    with open(filename, 'wb') as dump:
+        pickle.dump(obj, dump)
 
 
 def vocab(xs, specials= "\xa0\n", min_freq= 2, top= 256):
