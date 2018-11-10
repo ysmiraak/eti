3ad41ffccaf9e999af065356749e43e811e7064c
ysmiraak <ysmiraak@gmail.com>
2018-11-10 07:11:57 +0100

    use sentence piece

--- src/util_io.py
+++ src/util_io.py
@@ -1,8 +1,6 @@
-from collections import Counter
 from os.path import expanduser, join
 from util import Record
 import pickle
-import re
 
 
 path = Record(
@@ -12,9 +10,6 @@ path = Record(
     , pred = "../trial/pred"
     , ckpt = "../trial/ckpt"
     , data = "../trial/data"
-    , idx = "idx.pkl"
-    , src = "src.txt"
-    , tgt = "tgt.txt"
 )
 
 
@@ -23,39 +18,10 @@ def pform(path, *names, sep= ''):
     return join(path, sep.join(map(str, names)))
 
 
-def sieve(src_tgt, cap):
-    """removes extra whitespaces in strs and strs longer than `cap` in
-    `src_tgt : seq (str, str)`.
-
-    """
-    for src, tgt in src_tgt:
-        src = " ".join(src.split())
-        tgt = " ".join(tgt.split())
-        if not src or not tgt \
-           or cap < len(src) \
-           or cap < len(tgt):
-            continue
-        yield src, tgt
-
-
-def clean(s, p= re.compile(r"&#91;|&#93;|&amp;|&apos;|&gt;|&lt;|&quot;|@-@|� s")
-          , tr= lambda m, t= {
-              "� s": "'s"
-              , "&#91;": "["
-              , "&#93;": "]"
-              , "&amp;": "&"
-              , "&apos;": "'"
-              , "&gt;": ">"
-              , "&lt;": "<"
-              , "&quot;": "\""
-              , "@-@": "-"}: t[m.group()]):
-    return p.sub(tr, s)
-
-
 def load_txt(filename):
     """yields lines from text file."""
     with open(filename) as file:
-        yield from (line for line in file)
+        yield from (line[:-1] for line in file)
 
 
 def save_txt(filename, lines):
@@ -75,24 +41,3 @@ def save_pkl(filename, obj):
     "saves to pickle file."
     with open(filename, 'wb') as dump:
         pickle.dump(obj, dump)
-
-
-def vocab(xs, specials= "\xa0\n ", min_freq= 2, top= 256):
-    """-> (list a) where
-
-    xs       : seq a
-    specials : seq a
-    min_freq : nat
-    top      : nat | None
-
-    returns the `top` most frequent items with `min_freq` in `xs`, and
-    ensures that the `specials` are included with the highest ranks.
-
-    """
-    freq = Counter(xs)
-    for x in specials: del freq[x]
-    freq = [(-n, x) for x, n in freq.items() if min_freq <= n]
-    freq.sort()
-    vocab = list(specials)
-    vocab.extend((x for _, x in freq))
-    return vocab[:top] if top else vocab
