b42b397cfad5e2f17c9c52ba44d61749b84a3356
ysmiraak <ysmiraak@gmail.com>
2018-11-10 09:47:39 +0100

    fix empty seq bug

--- src/trial.py
+++ src/trial.py
@@ -13,11 +13,8 @@ master = Record(
     ### model spec
     , dim_src = 8192
     , dim_tgt = 8192
-    # model dimension
     , dim_emb = 512
-    # mlp middle layer dimension
     , dim_mid = 1024
-    # encoder and decoder depth
     , depth   = 2
     ### regularization
     , dropout = 0.1
@@ -28,12 +25,8 @@ master = Record(
     , beta2   = 0.98
     , epsilon = 1e-9
     ### training schedule
-    , shuffle = 2**14
-    # batch size for training
     , batch_train = 64
-    # batch size for validation
     , batch_valid = 512
-    # instances for validation
     , total_valid = 2560
 )
 
