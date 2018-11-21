9a8536d29442345f06fa3fbc8343f89e3aad581a
ysmiraak <ysmiraak@gmail.com>
2018-11-21 17:06:46 +0000

    minor

--- src/bleu.sh
+++ src/bleu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # pip install --user sacrebleu
-# ~/.local/bin/sacrebleu --test-set wmt08/europarl --language-pair de-en --echo src > ../trial/data/test.txt
+# sacrebleu --test-set wmt08/europarl --language-pair de-en --echo src > ../trial/data/test.txt
 
-~/.local/bin/sacrebleu --input $1 ../trial/data/valid_tgt.txt
+sacrebleu --input $1 ../trial/data/valid_tgt.txt
