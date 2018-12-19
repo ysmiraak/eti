f3f2339a86bc954f14fea404e8de33593fb40ceb
ysmiraak <ysmiraak@gmail.com>
2018-12-19 20:00:10 +0100

    data prep done

--- src/data.py
+++ src/data.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 from collections import defaultdict
-from itertools import chain
 from trial import config as C, paths as P, train as T
 from util_io import pform, load_txt, save_txt
 from util_np import np, vpack
-from util_sp import spm, load_spm
+from util_sp import spm, load_spm, encode
 
 # train one spm for each lang
 vocab_da = spm(pform(P.data, "vocab_da"), pform(P.raw, "europarl-v7.da-en.da"), C.dim_voc, C.bos, C.eos, C.unk)
@@ -85,17 +84,70 @@ da = {t[0] for t in test}
 de = {t[1] for t in test}
 sv = {t[2] for t in test}
 nl = {t[3] for t in test}
-save_txt(pform(P.data, "nont_da-da"), [s for i, s in enumerate(da_da) if i not in da])
-save_txt(pform(P.data, "nont_da-en"), [s for i, s in enumerate(da_en) if i not in da])
-save_txt(pform(P.data, "nont_de-de"), [s for i, s in enumerate(de_de) if i not in de])
-save_txt(pform(P.data, "nont_de-en"), [s for i, s in enumerate(de_en) if i not in de])
-save_txt(pform(P.data, "nont_sv-sv"), [s for i, s in enumerate(sv_sv) if i not in sv])
-save_txt(pform(P.data, "nont_sv-en"), [s for i, s in enumerate(sv_en) if i not in sv])
-save_txt(pform(P.data, "nont_nl-nl"), [s for i, s in enumerate(nl_nl) if i not in nl])
-save_txt(pform(P.data, "nont_nl-en"), [s for i, s in enumerate(nl_en) if i not in nl])
+da_da = [s for i, s in enumerate(da_da) if i not in da]
+da_en = [s for i, s in enumerate(da_en) if i not in da]
+de_de = [s for i, s in enumerate(de_de) if i not in de]
+de_en = [s for i, s in enumerate(de_en) if i not in de]
+sv_sv = [s for i, s in enumerate(sv_sv) if i not in sv]
+sv_en = [s for i, s in enumerate(sv_en) if i not in sv]
+nl_nl = [s for i, s in enumerate(nl_nl) if i not in nl]
+nl_en = [s for i, s in enumerate(nl_en) if i not in nl]
 
-# todo
-# - merge training data from da de sv
-# - merge training data from nl
-# - split validation data for da de sv
-# - split validation data for nl
+np.random.seed(1)
+np.random.shuffle(da_da)
+np.random.seed(1)
+np.random.shuffle(da_en)
+np.random.seed(2)
+np.random.shuffle(de_de)
+np.random.seed(2)
+np.random.shuffle(de_en)
+np.random.seed(3)
+np.random.shuffle(sv_sv)
+np.random.seed(3)
+np.random.shuffle(sv_en)
+np.random.seed(4)
+np.random.shuffle(nl_nl)
+np.random.seed(4)
+np.random.shuffle(nl_en)
+
+valid_da_da = da_da[:1024]
+valid_da_en = da_en[:1024]
+valid_de_de = de_de[:1024]
+valid_de_en = de_en[:1024]
+valid_sv_sv = sv_sv[:1024]
+valid_sv_en = sv_en[:1024]
+valid_nl_nl = nl_nl[:2048]
+valid_nl_en = nl_en[:2048]
+
+train_da_da = da_da[1024:]
+train_da_en = da_en[1024:]
+train_de_de = de_de[1024:]
+train_de_en = de_en[1024:]
+train_sv_sv = sv_sv[1024:]
+train_sv_en = sv_en[1024:]
+train_nl_nl = nl_nl[2048:]
+train_nl_en = nl_en[2048:]
+
+vocab_en = load_spm(pform(P.data, "vocab_en.model"))
+vocab_da = load_spm(pform(P.data, "vocab_da.model"))
+vocab_de = load_spm(pform(P.data, "vocab_de.model"))
+vocab_sv = load_spm(pform(P.data, "vocab_sv.model"))
+vocab_nl = load_spm(pform(P.data, "vocab_nl.model"))
+
+np.save(pform(P.data, "valid_da_da.npy"), encode(vocab_da, valid_da_da, length= C.cap, dtype= np.uint16))
+np.save(pform(P.data, "valid_da_en.npy"), encode(vocab_en, valid_da_en, length= C.cap, dtype= np.uint16))
+np.save(pform(P.data, "valid_de_de.npy"), encode(vocab_de, valid_de_de, length= C.cap, dtype= np.uint16))
+np.save(pform(P.data, "valid_de_en.npy"), encode(vocab_en, valid_de_en, length= C.cap, dtype= np.uint16))
+np.save(pform(P.data, "valid_sv_sv.npy"), encode(vocab_sv, valid_sv_sv, length= C.cap, dtype= np.uint16))
+np.save(pform(P.data, "valid_sv_en.npy"), encode(vocab_en, valid_sv_en, length= C.cap, dtype= np.uint16))
+np.save(pform(P.data, "valid_nl_nl.npy"), encode(vocab_nl, valid_nl_nl, length= C.cap, dtype= np.uint16))
+np.save(pform(P.data, "valid_nl_en.npy"), encode(vocab_en, valid_nl_en, length= C.cap, dtype= np.uint16))
+
+np.save(pform(P.data, "train_da_da.npy"), encode(vocab_da, train_da_da, length= C.cap, dtype= np.uint16))
+np.save(pform(P.data, "train_da_en.npy"), encode(vocab_en, train_da_en, length= C.cap, dtype= np.uint16))
+np.save(pform(P.data, "train_de_de.npy"), encode(vocab_de, train_de_de, length= C.cap, dtype= np.uint16))
+np.save(pform(P.data, "train_de_en.npy"), encode(vocab_en, train_de_en, length= C.cap, dtype= np.uint16))
+np.save(pform(P.data, "train_sv_sv.npy"), encode(vocab_sv, train_sv_sv, length= C.cap, dtype= np.uint16))
+np.save(pform(P.data, "train_sv_en.npy"), encode(vocab_en, train_sv_en, length= C.cap, dtype= np.uint16))
+np.save(pform(P.data, "train_nl_nl.npy"), encode(vocab_nl, train_nl_nl, length= C.cap, dtype= np.uint16))
+np.save(pform(P.data, "train_nl_en.npy"), encode(vocab_en, train_nl_en, length= C.cap, dtype= np.uint16))
