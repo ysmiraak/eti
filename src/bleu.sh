1a10f8c311c837c7cbf395ed0b06887d3560c862
ysmiraak <ysmiraak@gmail.com>
2019-01-14 13:25:45 +0100

    use intl tokenizer for bleu

--- src/bleu.sh
+++ src/bleu.sh
@@ -3,4 +3,4 @@
 # pip install --user sacrebleu
 # sacrebleu -t wmt08/europarl -l de-en --echo src > ../trial/data/test.txt
 
-sacrebleu -b -i $1 ../trial/data/valid_tgt.txt
+sacrebleu -tok intl -b -i $1 ../trial/data/valid_tgt.txt
